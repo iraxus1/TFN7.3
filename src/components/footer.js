345e4b3db921dc810f268f1834af9382d22d2ac1
iraxus1 <76206932+iraxus1@users.noreply.github.com>
2022-11-27 19:07:45 +0100

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
     return (
         <footer>
-            <p>Copyright by Technologie Frontendowe</p>
+            <p>Copyright by Bogate Interfejsy Webowe</p>
         </footer>
     );
 }
